0e34bc7613b4376520d9e5a72d4d4dd4623702d7
Rogier Slag <rogier.slag@gmail.com>
2016-04-21 14:18:13 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+app.get('/', function (req, res) {
+	res.statusCode = 503;
+	res.write('No NGINX. I dont work, but I dont want an empty upstream block either');
+	res.end();
+	
+});
+app.listen(port, function() {
+	console.log('Nginx 503 generator started at port ' + port);
+});
+
