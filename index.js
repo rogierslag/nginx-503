7e7bec2796901886c43d6bcd463d52c3ab242f1e
Rogier Slag <rogier.slag@gmail.com>
2016-08-19 10:20:47 +0200

    Include logging

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 
 var app = express();
+
+app.enable('trust proxy');
 app.use('/*', function (req, res) {
-	res.statusCode = 503;
-	res.write('No NGINX. I dont work, but I dont want an empty upstream block either');
-	res.end();
-	
+	res.status(503).end('This service is temporarily unavailable');
+	console.log(JSON.stringify({host: req.headers.host, datetime: (new Date()).toISOString(), client: req.ip}));
 });
 app.listen(port, function() {
 	console.log('Nginx 503 generator started at port ' + port);
